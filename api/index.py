ccb3e5ae3d35b7edc7c44f1687e3dc4cedb8d5d0
Jurica Migac <jurica1.migac@gmail.com>
2024-03-20 21:03:58 +0100

    Implementation of caching layer

--- api/index.py
+++ api/index.py
@@ -1,11 +1,61 @@
-from flask import Flask
+import json
+import os
+import requests
+from datetime import datetime
+from flask import Flask, Response
+from api.models.Cache import Cache
 
+environment = os.environ['ENVIRONMENT']
+space_id = os.environ['SPACE_ID']
+token = os.environ['APP_TOKEN']
+cache_duration = os.environ['CACHE_DURATION']
 app = Flask(__name__)
+cache = Cache(cache_duration)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/experiences')
+def experiences():
+    experience_articles = ""
+    if cache.is_cache_expired() or not cache.experiences:
+        url = f"https://graphql.contentful.com/content/v1/spaces/{space_id}/environments/{environment}"
+        payload = "{\"query\":\"{\\r\\n  experienceArticleCollection(limit: 8) {\\r\\n    items {\\r\\n      title\\r\\n      description\\r\\n      from\\r\\n      until\\r\\n    }\\r\\n  }\\r\\n}\",\"variables\":{}}"
+        headers = {
+          'Content-Type': 'application/json',
+          'Authorization': f"Bearer {token}"
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        response_data = response.json()
+        response_field = response_data['data']
+        response_collection = response_field['experienceArticleCollection']
+        response_collection_items = response_collection['items']
+        experience_articles = response_collection_items
+        cache.cache_time = datetime.now()
+        cache.experiences = response_collection_items
+    else:
+        experience_articles = cache.experiences
+    return Response(json.dumps(experience_articles, indent=4), mimetype="application/json")
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/projects')
+def projects():
+    projects = ""
+    if cache.is_cache_expired() or not cache.projects:
+        url = f"https://graphql.contentful.com/content/v1/spaces/{space_id}/environments/{environment}"
+        payload = "{\"query\":\"{\\r\\n  projectArticleCollection(limit: 8) {\\r\\n    items {\\r\\n      title\\r\\n      description\\r\\n      technologies\\r\\n    }\\r\\n  }\\r\\n}\",\"variables\":{}}"
+        headers = {
+          'Content-Type': 'application/json',
+          'Authorization': f"Bearer {token}"
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        response_data = response.json()
+        response_field = response_data['data']
+        response_collection = response_field['projectArticleCollection']
+        response_collection_items = response_collection['items']
+        projects = response_collection_items
+        cache.cache_time = datetime.now()
+        cache.projects = response_collection_items
+    else:
+        projects = cache.projects
+    return Response(json.dumps(projects, indent=4), mimetype="application/json")
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ api/models/Cache.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+class Cache:
+
+    def __init__(self, cache_duration):
+        self.cache_time = datetime.now()
+        self.experiences = ""
+        self.projects = []
+        self.cache_duration = cache_duration
+
+    def is_cache_expired(self):
+        current_time = datetime.now()
+        difference = current_time - self.cache_time
+        return difference.seconds > int(self.cache_duration)
