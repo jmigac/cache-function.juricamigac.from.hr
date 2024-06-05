6456c6178f96c7c7205b5bf54dfbca2020f35cc2
Jurica Migac <jurica1.migac@gmail.com>
2024-06-05 20:59:32 +0200

    Removal of Glucose endpoints

--- api/index.py
+++ api/index.py
@@ -6,22 +6,16 @@ from api.models.Cache import Cache
 from api.models.contentful_request import ContentfulRequest
 from api.constants.constants import (APPLICATION_JSON, ENVIRONMENT, SPACE_ID,
                                      APP_TOKEN, CACHE_DURATION, INVALIDATION_TOKEN,
-                                     ACCESS_CONTROL_ALLOW_ORIGIN, QUERY_TOKEN, SUPABASE_URL,
-                                     SUPABASE_KEY)
-from supabase import create_client, Client
+                                     ACCESS_CONTROL_ALLOW_ORIGIN, QUERY_TOKEN)
 
 environment = os.environ[ENVIRONMENT]
 space_id = os.environ[SPACE_ID]
 token = os.environ[APP_TOKEN]
 cache_duration = os.environ[CACHE_DURATION]
-invalidation_token = 323
+invalidation_token = os.environ[INVALIDATION_TOKEN]
 app = Flask(__name__)
 cache = Cache(cache_duration)
 contentful = ContentfulRequest(space_id, environment, token)
-
-url = os.environ.get(SUPABASE_URL)
-key = os.environ.get(SUPABASE_KEY)
-Client = create_client(url, key)
 
 
 @app.route('/experiences')
@@ -53,6 +47,7 @@ def projects():
                     headers=ACCESS_CONTROL_ALLOW_ORIGIN,
                     mimetype=APPLICATION_JSON)
 
+
 @app.route('/homePage')
 def get_home_page():
     home_page_result = ""
@@ -82,22 +77,3 @@ def invalidate():
                     headers=ACCESS_CONTROL_ALLOW_ORIGIN,
                     mimetype=APPLICATION_JSON)
 
-
-@app.route("/glucose/<glucose_value>")
-def insert_glucose_value(glucose_value):
-    Client.table('glucose').insert({"value": glucose_value}).execute()
-    return Response("Glucose level successfully inserted",
-                    headers=ACCESS_CONTROL_ALLOW_ORIGIN,
-                    mimetype=APPLICATION_JSON)
-
-
-@app.route("/glucose/latest")
-def get_latest():
-    data, count = Client.table('glucose').select('*').order('date', desc=True).limit(1).single().execute()
-    data_json = json.dumps(data[1], indent=4)
-    return Response(data_json,
-                    headers=ACCESS_CONTROL_ALLOW_ORIGIN,
-                    mimetype=APPLICATION_JSON)
-
-if __name__ == "__main__":
-    app.run()
